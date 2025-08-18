c1df27bc268fa37c316f65e969813badcfa57e83
Fellteros <144528463+Fellteros@users.noreply.github.com>
2025-08-18 19:32:55 +0200

    Checkstyle

--- fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/event/interaction/ExperienceOrbEntityMixin.java
+++ fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/event/interaction/ExperienceOrbEntityMixin.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.mixin.event.interaction;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,7 +30,7 @@ import net.fabricmc.fabric.api.event.player.PlayerXpEvents;
 public class ExperienceOrbEntityMixin {
 	@Inject(method = "onPlayerCollision", at = @At("HEAD"), cancellable = true)
 	private void fabric_onPlayerCollision(PlayerEntity player, CallbackInfo ci) {
-		boolean	processFurther = PlayerXpEvents.PICKUP_XP.invoker().onPickup(player, (ExperienceOrbEntity) (Object) this);
+		boolean processFurther = PlayerXpEvents.PICKUP_XP.invoker().onPickup(player, (ExperienceOrbEntity) (Object) this);
 
 		if (!processFurther) {
 			ci.cancel();
--- fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/event/interaction/PlayerEntityMixin.java
+++ fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/event/interaction/PlayerEntityMixin.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.mixin.event.interaction;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,7 +29,7 @@ import net.fabricmc.fabric.api.event.player.PlayerXpEvents;
 public class PlayerEntityMixin {
 	@Inject(method = "addExperience", at = @At("HEAD"), cancellable = true)
 	private void fabric_addExperience(int experience, CallbackInfo ci) {
-		boolean	processFurther = PlayerXpEvents.XP_CHANGE.invoker().onXpChange((PlayerEntity) (Object) this, experience);
+		boolean processFurther = PlayerXpEvents.XP_CHANGE.invoker().onXpChange((PlayerEntity) (Object) this, experience);
 
 		if (!processFurther) {
 			ci.cancel();
@@ -22,7 +38,7 @@ public class PlayerEntityMixin {
 
 	@Inject(method = "addExperienceLevels", at = @At("HEAD"), cancellable = true)
 	private void fabric_addExperienceLevels(int levels, CallbackInfo ci) {
-		boolean	processFurther = PlayerXpEvents.LEVEL_CHANGE.invoker().onLevelChange((PlayerEntity) (Object) this, levels);
+		boolean processFurther = PlayerXpEvents.LEVEL_CHANGE.invoker().onLevelChange((PlayerEntity) (Object) this, levels);
 
 		if (!processFurther) {
 			ci.cancel();
